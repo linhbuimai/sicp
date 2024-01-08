3a963c6e850ed4b0a0a2e75912816d42f91da7c8
Bui Mai Linh <linhmai236@outlook.com>
2024-01-08 19:05:17 +0700

    Sync

--- simply-scheme/ch01_functions.rkt
+++ simply-scheme/ch01_functions.rkt
@@ -1,14 +1,56 @@
 (require (planet dyoo/simply-scheme:1:2/simply-scheme))
 
-(define (acronym phrase)
-  (accumulate word (every first phrase)))
-
-(acronym '(american civil liberties union))
-(every first '(american civil liberties union))
-
-(acronym '(structure and interpretation of computer programs))
-
+; Simple functions examples
 (define (real-word? wd)
-  (not (member? wd '(a the an in of and for to with))))
+  (not (member? wd '(a the an in of and for  to with))))
 
-(acronym '(structure and interpretation of computer programs))
+(define (acronym phrase)
+  (accumulate word (every first (keep real-word? phrase))))
+
+(acronym '(united states of america))
+
+; Example: Latin Pig
+
+(define (pigl wd)
+  (if (member? (first wd) 'aeiou)
+      (word wd 'ay)
+      (pigl (word (butfirst wd)(first wd)))))
+
+(pigl 'spaghetti)
+(pigl 'ok)
+(every pigl '(the ballad of john and yoko))
+
+; Example: Ice Cream Choices
+(define (choices menu)
+  (if (null? menu)
+      '(())
+      (let ((smaller (choices (cdr menu))))
+        (reduce append
+                (map (lambda (item) (prepend-every item smaller))
+                     (car menu))))))
+
+(define (prepend-every item lst)
+  (map (lambda (choice) (se item choice)) lst))
+
+(choices '((small medium large)
+           (vanilla (ultra chocolate) (rum raisin) ginger)
+           (cone cup)) )
+
+; Example: Combinations from a  Set
+(define (combinations size set)
+  (cond ((= size 0) '(()))
+        ((empty? set) '())
+        (else (append (prepend-every (first set)
+                                     (combinations (- size 1)
+                                                  (butfirst set)))
+                      (combinations size (butfirst set))))))
+
+(combinations 3 '(a b c d e))
+
+; Example: Factorial
+(define (factorial n)
+  (if (= n 0)
+      1
+      (* n (factorial (- n 1)))))
+
+(factorial 5)
