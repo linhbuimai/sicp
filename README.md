a9e5b0eb9321815f3845b881e48c858341cfd2f2
Bui Mai Linh <linhmai236@outlook.com>
2024-10-24 19:29:51 +0700

    Add chapter note 09

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # SICP Learning Path
 
 "The structure and interpretation of computer programs"
+
+# Pre-defined functions
+
+> in the Simply Scheme book
+
+```scheme
+word
+keep
+accumulate
+every
+first
+butfirst (remove first character/item) (bf)
+butlast (bl)
+append
+prepend-every
+
+; to check type
+number?
+boolean?
+word?
+sentence?
+
+; other predicates
+even? 
+odd?
+equal?
+member?
+before?
+empty>
+```
--- simply-scheme/chapter_note/ch09_lambda.rkt
+++ simply-scheme/chapter_note/ch09_lambda.rkt
@@ -15,3 +15,27 @@ What: Lambda (anonymous function)
   (map func lst))
 
 (apply-func (lambda (x) (+ x 3)) '(1 2 3 4))
+
+(every (lambda (wd) (se (first wd) wd (last wd)))
+  '(only a northern song)
+)
+
+(keep (lambda (n) (member? 9 n)) '(4 81 909 1969 1776))
+
+(accumulate (lambda (this that)
+              (if (> (count this) (count that)) this that))
+            '(wild honey pie))
+
+"""
+Use lambda to write function that returns funtion
+"""
+(define (make-adder num) (lambda (x) (+ x num)))
+
+; comment:
+; when call (make-adder 4) --> return a fuction that will add 4 to some value
+; ((make-adder 4) 7) <-- add 4 to 7 
+
+;; the below procedure receive another procedure fn
+(define (same-arg-twice fn) (lambda (arg) (fn arg arg)))
+
+(define (flip fn) (lambda (a b) (fn b a)))
