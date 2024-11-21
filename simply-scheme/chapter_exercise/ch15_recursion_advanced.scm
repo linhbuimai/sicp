8f84f47a8566e836ebeb6e4aab81ac59504d7e54
Bui Mai Linh <linhmai236@outlook.com>
2024-11-21 18:12:09 +0700

    Sync exercise 15

---
+++ simply-scheme/chapter_exercise/ch15_recursion_advanced.scm
@@ -0,0 +1,61 @@
+(require (planet dyoo/simply-scheme:1:2/simply-scheme))
+
+;; 15.1
+
+(define (to-binary-helper num wd)
+  (if (<= num 1)
+    (word num wd)
+    (word (to-binary-helper (quotient num 2) (word (remainder num 2) wd)))
+  )
+)
+
+(define (to-binary num)
+  (to-binary-helper num "")
+)
+
+;; 15.2
+
+(define (combine-sentence sent wd)
+  (if (empty? sent)
+    wd
+    (combine-sentence (bf sent) (word wd (first sent)))
+  )
+)
+
+(define (palindrome-helper wd)
+  (cond
+    ((<= (count wd) 1) #t)
+    ((equal? (first wd) (last wd)) (palindrome-helper (bf (bl wd))))
+    (else #f)
+  )
+)
+
+(define (palindrome? sent)
+  (let ((wd (combine-sentence sent "")))
+    (palindrome-helper wd)
+  )
+)
+
+;; 15.3
+
+(define (prepend-every letter sent)
+  (if (empty? sent)
+    (se letter '())
+    (se (word letter (first sent))
+      (prepend-every letter (bf sent)) 
+    )
+  )
+)
+
+(define (substrings wd)
+  (cond 
+    ((= (count wd) 1) (se wd))
+    ((= (count wd) 2) (se (first wd) wd (last wd)))
+    (else
+      (se 
+        (substrings (bf wd))
+        (prepend-every (first wd) (bl (substrings (bf wd))))
+      )
+    )
+  )
+)
--- simply-scheme/chapter_note/ch15_recursion_advanced.scm
+++ simply-scheme/chapter_note/ch15_recursion_advanced.scm
@@ -97,3 +97,20 @@ Example: Merge sort
 Example: Subsets
 """
 
+(define (prepend-every letter sent)
+  (if (empty? sent)
+    '()
+    (se (word letter (first sent))
+      (prepend-every letter (bf sent))
+    )
+  )
+)
+
+(define (subsets wd)
+  (if (empty? wd)
+    (se "")
+    (let ((smaller (subsets (bf wd))))
+      (se smaller (prepend-every (first wd) smaller))
+    )
+  )
+)
