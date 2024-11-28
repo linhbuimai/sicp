2b9352555efc2c41131bcccdc84475c45d0675ae
Bui Mai Linh <linhmai236@outlook.com>
2024-11-28 18:45:11 +0700

    Sync huhu

--- simply-scheme/chapter_exercise/ch15_recursion_advanced.scm
+++ simply-scheme/chapter_exercise/ch15_recursion_advanced.scm
@@ -37,22 +37,42 @@
 )
 
 ;; 15.3
+; procedure that returns all substrings of a word
 
-(define (prepend-every letter sent)
-  (if (empty? sent)
-    (se letter '())
-    (se (word letter (first sent))
-      (prepend-every letter (bf sent)) 
-    )
+(define (substrings-helper wd)
+  (if (empty? wd)
+    '()
+    (se wd (substrings-helper (bl wd)))
   )
 )
 
 (define (substrings wd)
-  (cond 
-    ((= (count wd) 1) (se wd))
-    ((= (count wd) 2) (se (first wd) wd (last wd)))
-    (else
-      
+  (if (empty? wd)
+    (se '())
+    (let ((var-substr (substrings-helper wd)))
+      (se var-substr (substrings (bf wd)))
     )
   )
 )
+
+;; 15.4
+(define (substrings? wd1 wd)
+  (let ((all-substr (substrings wd)))
+    (member? wd1 all-substr)
+  )
+)
+
+(substrings? 'ssip 'mississippi)
+(substrings? 'misip 'mississippi)
+
+;; 15.5
+(define (letters n)
+  (cond ((= n 2) (se 'a 'b 'c))
+        ((= n 3) (se 'd 'e 'f))
+        ((= n 4) (se 'g 'h 'i))
+        ((= n 5) (se 'j 'k 'l))
+        ((= n 6) (se 'm 'n 'o))
+        ((= n 7) (se 'p 'q 'r 's))
+        ((= n 8) (se 't 'u 'v))
+        ((= n 9) (se 'w 'x 'y 'z))
+        (else n)))
